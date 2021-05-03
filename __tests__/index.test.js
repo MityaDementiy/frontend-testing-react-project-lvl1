5dac2993b020b0607f519235dcb27dda9da840b9
MityaDementiy <d.dementiy@gmail.com>
2021-05-03 18:47:36 +0300

    Use tempdir in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,6 @@
 import nock from 'nock';
+import os from 'os';
+import { promises as fs } from 'fs';
 
 import loader from '../src/index';
 
@@ -14,8 +16,8 @@ afterAll(() => {
 test('returns filepath', async () => {
   nock('https://ru.hexlet.io').get('/courses').reply(200);
 
-  const result = await loader('https://ru.hexlet.io/courses', 'downloads');
-  const expected = 'downloads/ru-hexlet-io-courses.html';
+  const result = await loader('https://ru.hexlet.io/courses', os.tmpdir());
+  const expected = '/tmp/ru-hexlet-io-courses.html';
 
   expect(result).toEqual(expected);
 });
