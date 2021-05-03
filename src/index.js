725738d0fad7d6255bfcb58fb68a87b3336a57a2
MityaDementiy <d.dementiy@gmail.com>
2021-05-03 15:11:16 +0300

    Add possibility to load a page to cwd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+import axios from 'axios';
+import { promises as fs } from 'fs';
+
 import formatUrl from './utils';
 
-const loader = (url) => formatUrl(url);
+const loader = async (url) => {
+  const filePath = formatUrl(url);
+
+  const pageData = await axios.get(url)
+    .then((response) => response.data)
+    .then((data) => data)
+    .catch((err) => err.message);
+
+  try {
+    fs.writeFile(filePath, pageData);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return filePath;
+};
 
 export default loader;
