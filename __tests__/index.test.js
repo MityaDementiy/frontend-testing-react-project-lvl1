cafdad5e4a26d9b69bbbe8e549c2da00769072ce
MityaDementiy <d.dementiy@gmail.com>
2021-05-03 19:13:05 +0300

    Test loader

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
 import nock from 'nock';
 import os from 'os';
 import { promises as fs } from 'fs';
+import path from 'path';
 
 import loader from '../src/index';
 
@@ -13,11 +14,31 @@ afterAll(() => {
   nock.enableNetConnect();
 });
 
+beforeEach(async () => {
+  await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+
+afterEach(async () => {
+  await fs.rmdir('/tmp/page-loader-', { recursive: true, force: true });
+});
+
+const getFixturePath = (name) => path.join('__fixtures__', name);
+
 test('returns filepath', async () => {
   nock('https://ru.hexlet.io').get('/courses').reply(200);
 
-  const result = await loader('https://ru.hexlet.io/courses', os.tmpdir());
-  const expected = '/tmp/ru-hexlet-io-courses.html';
+  const result = await loader('https://ru.hexlet.io/courses', '/tmp/page-loader-');
+  const expected = '/tmp/page-loader-/ru-hexlet-io-courses.html';
 
   expect(result).toEqual(expected);
+});
+
+test('loads page', async () => {
+  const data = await fs.readFile(getFixturePath('ru-hexlet-io-courses.html'), 'utf-8');
+  nock('https://ru.hexlet.io').get('/courses').reply(200, data);
+
+  await loader('https://ru.hexlet.io/courses', '/tmp/page-loader-');
+  const result = await fs.readFile('/tmp/page-loader-/ru-hexlet-io-courses.html', 'utf-8');
+
+  expect(result).toEqual(data);
 });
