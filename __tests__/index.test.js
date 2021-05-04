7ab54f59713a12f4a06875a8f03fa2e521dcffc9
MityaDementiy <d.dementiy@gmail.com>
2021-05-04 09:26:20 +0300

    Fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -20,12 +20,13 @@ beforeEach(async () => {
   tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test('returns filepath', async () => {
-  const scope = nock('https://ru.hexlet.io').get('/courses').reply(200);
+test('test loader', async () => {
+  const scope = nock('https://ru.hexlet.io').get('/courses').reply(200, 'hello');
 
   const result = await loader('https://ru.hexlet.io/courses', tempDir);
   const expected = `${tempDir}/ru-hexlet-io-courses.html`;
 
   expect(result).toEqual(expected);
   expect(scope.isDone).toBeTruthy();
+  expect(result.endsWith('ru-hexlet-io-courses.html')).toBe(true);
 });
