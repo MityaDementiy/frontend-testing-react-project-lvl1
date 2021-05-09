8e926b2687c1b08e55c36248115bcb376a76bd43
MityaDementiy <d.dementiy@gmail.com>
2021-05-09 17:48:43 +0300

    Refactor test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -23,15 +23,12 @@ beforeEach(async () => {
 const getFixturePath = (name) => path.join('__fixtures__', name);
 
 test('test loader', async () => {
-  const data = await fs.readFile(getFixturePath('ru-hexlet-io-courses.html'), 'utf-8');
-  const scope = nock('https://ru.hexlet.io').get('/courses').reply(200, data);
-
+  const processedData = await fs.readFile(getFixturePath('ru-hexlet-io-courses-expected.html'), 'utf-8');
+  const scope = nock('https://ru.hexlet.io').get('/courses').reply(200, processedData);
   const result = await loader('https://ru.hexlet.io/courses', tempDir);
-  
   const expected = `${tempDir}/ru-hexlet-io-courses.html`;
-  
+
   expect(result).toEqual(expected);
   expect(scope.isDone).toBeTruthy();
   expect(result.endsWith('ru-hexlet-io-courses.html')).toBe(true);
 });
-
