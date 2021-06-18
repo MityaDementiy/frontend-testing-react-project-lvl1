8a07ce59f9aea670d8ac92e59f20a9982cd88f75
MityaDementiy <d.dementiy@gmail.com>
2021-06-18 18:58:20 +0300

    Test urlToDirname utility function

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -21,5 +21,19 @@ describe('test utils', () => {
     expect(firstProcessedUrl).toEqual(firstExpectedUrl);
     expect(secondProcessedUrl).toEqual(secondExpectedUrl);
   });
+
+  test('urlToDirname should process urls', () => {
+    const firstUrl = 'https://ru.hexlet.io/programs/frontend-testing-react';
+    const firstExpectedUrl = 'https-ru-hexlet-io-programs-frontend-testing-react_files';
+    const secondUrl = 'http://www.ru.hexlet.io/programs/frontend-testing-react';
+    const secondExpectedUrl = 'http-www-ru-hexlet-io-programs-frontend-testing-react_files';
+
+    const firstProcessedUrl = urlToDirname(firstUrl);
+    const secondProcessedUrl = urlToDirname(secondUrl);
+
+    expect(firstUrl).not.toEqual(firstProcessedUrl);
+    expect(firstProcessedUrl).toEqual(firstExpectedUrl);
+    expect(secondProcessedUrl).toEqual(secondExpectedUrl);
+  });
 });
 
