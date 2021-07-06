44ca0bce0f64991e0019520df2fce9be5c9f4fa2
MityaDementiy <d.dementiy@gmail.com>
2021-07-06 08:55:17 +0300

    Fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -115,13 +115,12 @@ describe('test pageloader', () => {
     const imagePath = getFixturePath(expectedImage);
     const imageData = await fs.readFile(imagePath, 'utf-8');
     const sysDirPath = '/sys';
-    const firstErrorMessage = new RegExp('EACCES: permission denied');
-    const secondErrorMessage = new RegExp('EROFS: read-only file system');
+    const errorMessage = /EROFS: read-only file system|EACCES: permission denied/i;
 
     nock('https://ru.hexlet.io').get('/courses').reply(200, rawData);
     nock('https://ru.hexlet.io').get('/assets/professions/nodejs.png')
       .reply(200, imageData);
 
-    await expect(loader('https://ru.hexlet.io/courses', sysDirPath)).rejects.toThrow(firstErrorMessage || secondErrorMessage);
+    await expect(loader('https://ru.hexlet.io/courses', sysDirPath)).rejects.toThrow(errorMessage);
   });
 });
