0ca6d24d0ed2ec06be44478796c6eceb958f9045
MityaDementiy <d.dementiy@gmail.com>
2021-07-31 14:23:42 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -122,7 +122,7 @@ describe('test pageloader — negative cases', () => {
     scope.get(pagePath).reply(200);
 
     await expect(loader(requestUrl, incorrectDirPath)).rejects.toThrow(/ENOENT/);
-    await expect(loader(requestUrl, '/sys')).rejects.toThrow(/EACCES || EROFS/);
+    await expect(loader(requestUrl, '///////////////////////////////////////////test')).rejects.toThrow(/EACCES || EROFS/);
   });
 
   it('should throw when not directory', async () => {
